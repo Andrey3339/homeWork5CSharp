adaa0f58bd13b93cc1928ede8867905323bac0dd
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-12 18:18:18 +0300

    add task36

---
+++ task36/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); // [1, 10]
+}
+void CountArray(int[] array)
+{
+  int sum = 0;
+  for (int i = 1; i < array.Length; i = i + 2)
+  {
+    sum += array[i];
+  }
+    Console.WriteLine("Сумма элементов, стоящих на нечетных позициях - " + sum);
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов масссива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Сформированный массив: [{string.Join(", ", array)}]");
+CountArray(array);

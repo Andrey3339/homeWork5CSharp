afde9c163993625996a271b32d61104a65e0dc17
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-02-12 18:54:14 +0300

    add task38

---
+++ task38/Program.cs
@@ -0,0 +1,28 @@
+﻿void InputArray(double[] array)
+{
+  int begin = 1; 
+  int end = 99;
+  for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+}
+void CountArray(double[] array)
+{
+  double max = array[0];
+  double min = array[0];
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > max)
+      max = array[i];
+    if (array[i] < min)
+      min = array[i];
+  }
+    Console.WriteLine("Разница между max и min элементом: {0:F2}", (max - min));
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов масссива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine($"Сформированный массив: [{string.Join(", ", array)}]");
+CountArray(array);
+
